36ca50f2b9119cd7e7156a7ca6ed869caeaa7eca
ravi barla <ravibarla1@gmail.com>
2023-09-20 19:53:54 +0530

    using express validator

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,23 +1,25 @@
-const validateMiddleware = (req, res, next) => {
+import { body, validationResult } from "express-validator";
+
+const validateMiddleware = async (req, res, next) => {
   //validate data
-  const { name, desc, price, imageUrl } = req.body;
-  let errors = [];
-  if (!name || name.trim() == "") {
-    errors.push("name is required");
-  }
-  if (!desc || desc.trim() == "") {
-    errors.push("description is required");
-  }
-  if (!price || parseFloat(price) < 1) {
-    errors.push("price must be positive value");
-  }
-  try {
-    const validUrl = new URL(imageUrl);
-  } catch (err) {
-    errors.push("URL is invalid");
-  }
-  if (errors.length > 0) {
-    return res.render("new-product", { errorMessage: errors[0] });
+
+  //1. setup rules for validation
+  const rules = [
+    body("name").notEmpty().withMessage("name should be valid"),
+    body("price").isFloat({ gt: 0 }).withMessage("price should be positive"),
+    body("imageUrl").isURL().withMessage("url should be valid"),
+  ];
+  //2. run those rules
+  await Promise.all(rules.map((rule) => rule.run(req)));
+  //3. check if there are any errors after running  the rules
+
+  var validationErrors = validationResult(req);
+  console.log(validationErrors);
+  //if there is any error then return error msg
+  if (!validationErrors.isEmpty()) {
+    return res.render("new-product", {
+      errorMessage: validationErrors.array()[0].msg,
+    });
   }
 
   next();
