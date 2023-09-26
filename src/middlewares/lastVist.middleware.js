41e67e685bdbd82314d423e5a9d9f42fb2a20acb
ravi barla <ravibarla1@gmail.com>
2023-09-26 23:08:39 +0530

    remove comments

--- src/middlewares/lastVist.middleware.js
+++ src/middlewares/lastVist.middleware.js
@@ -1,9 +1,8 @@
 export const setLastVisit = (req, res, next) => {
   // 1. if cookies is set , then add a local variable with last visit time data
-  console.log("req.cookies.lastVist :", req.cookies.lastVist);
   if (req.cookies.lastVist) {
     res.locals.lastVist = new Date(req.cookies.lastVist).toLocaleString();
-    console.log("inside lastVist :");
+  
   }
   res.cookie("lastVist", new Date().toISOString(), {
     maxAge: 2 * 24 * 60 * 60 * 1000,
