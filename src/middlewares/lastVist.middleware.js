35fa282a6f05674c8aab606b3eb4059916ee5d11
ravi barla <ravibarla1@gmail.com>
2023-09-26 11:58:37 +0530

    fixing cookies validation bug

--- src/middlewares/lastVist.middleware.js
+++ src/middlewares/lastVist.middleware.js
@@ -1,7 +1,9 @@
 export const setLastVisit = (req, res, next) => {
   // 1. if cookies is set , then add a local variable with last visit time data
-  if (req.cookies.lastVisit) {
-    res.locals.lastVisit = new Date(req.cookies.lastVisit).toLocaleString();
+ 
+  if (req.cookies.lastVist) {
+    res.locals.lastVist = new Date(req.cookies.lastVist).toLocaleString();
+   
   }
   res.cookie("lastVist", new Date().toISOString(), {
     maxAge: 2 * 24 * 60 * 60 * 1000,
